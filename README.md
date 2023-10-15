34fc8f99cb1e08163ff4ea17cf07e964d878d96c
Marcus Perdue <marcusperduee@gmail.com>
2023-10-15 14:37:05 -0700

    implemented dynamic inquirer choices

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # Employee-Tracker-Project
 application to manage a company's employee database, using Node.js, Inquirer, and MySQL.
+project-name/
+│
+├── db/
+│   ├── schema.sql       # Contains the SQL commands for creating tables.
+│   └── seeds.sql        # Contains SQL commands for seeding initial data.
+│
+├── config/
+│   └── connection.js    # Contains the configuration and connection to the MySQL database.
+│
+├── models/
+│   ├── department.js    # Model file for department-related database operations.
+│   ├── role.js          # Model file for role-related database operations.
+│   └── employee.js      # Model file for employee-related database operations.
+│
+├── routes/
+│   ├── apiRoutes.js     # Contains API routes for CRUD operations.
+│   └── htmlRoutes.js    # Contains routes for rendering views/pages.
+│
+├── public/
+│   ├── css/
+│   │   └── styles.css   # Contains styles for your frontend.
+│   ├── js/
+│   │   └── scripts.js   # Contains JavaScript for frontend interactivity.
+│   └── images/          # Folder for any static images.
+│
+├── views/
+│   ├── layouts/
+│   │   └── main.handlebars  # Main layout file if you're using Handlebars or similar.
+│   ├── partials/           # Folder for Handlebars partials, if applicable.
+│   ├── index.handlebars    # Main view file (replace with your choice of templating engine).
+│   └── ...                 # Other view files.
+│
+├── node_modules/         # Contains all the npm packages/modules (added by `npm install`).
+│
+├── package.json          # Contains metadata about the project and lists dependencies.
+├── package-lock.json     # Describes the exact tree generated in node_modules.
+└── server.js             # Entry point for your application. Sets up server and routes.
--- index.js
+++ index.js
@@ -134,44 +134,103 @@ function addDepartment() {
 
 //------------------------ Add employee ------------------------
 function addEmployee() {
-    inquirer.prompt([
-        {
-            name: 'first_name',
-            type: 'input',
-            message: 'First name of the employee:',
-        },
-    ]).then(answers => {
-        employeeModel.add(answers, (err, results) => {
+    employeeModel.getAll((err, employees) => {
+        if (err) throw err;
+
+        roleModel.getAll((err, roles) => {
             if (err) throw err;
-            console.log('Employee added!');
-            startPrompt();
+
+            inquirer.prompt([
+                {
+                    name: 'first_name',
+                    type: 'input',
+                    message: 'First name of the employee:',
+                },
+                {
+                    name: 'last_name',
+                    type: 'input',
+                    message: 'Last name of the employee:',
+                },
+                {
+                    name: 'role_id',
+                    type: 'list',
+                    message: 'Select the role for this employee:',
+                    choices: roles.map(role => ({
+                        name: role.title,
+                        value: role.id
+                    })),
+                },
+                {
+                    name: 'manager_id',
+                    type: 'list',
+                    message: 'Select the manager for this employee (if applicable):',
+                    choices: employees.map(employee => ({
+                        name: `${employee.first_name} ${employee.last_name}`,
+                        value: employee.id
+                    })),
+                },
+            ]).then(answers => {
+                employeeModel.add(answers, (err, results) => {
+                    if (err) throw err;
+                    console.log('Employee added!');
+                    startPrompt();
+                });
+            });
         });
     });
 }
 
+
 //------------------------ Update employee ------------------------
+
 function updateEmployeeRole() {
-    inquirer.prompt([
-        {
-            name: 'employee_id',
-            type: 'list',
-            message: 'Which employee would you like to update?',
-            choices: [],  
-        },
-        {
-            name: 'role_id',
-            type: 'list',
-            message: 'What is the new role for this employee?',
-            choices: []  
-        }
-    ]).then(answers => {
-        employeeModel.updateRole(answers, (err, results) => {
+    employeeModel.getAll((err, employees) => {
+        if (err) throw err;
+
+        roleModel.getAll((err, roles) => {
             if (err) throw err;
-            console.log('Employee role updated!');
-            startPrompt();
+
+            inquirer.prompt([
+                {
+                    name: 'employee_id',
+                    type: 'list',
+                    message: 'Which employee would you like to update? (or select "Exit" to return to the main menu)',
+                    choices: [...employees.map(employee => ({
+                        name: `${employee.first_name} ${employee.last_name}`,
+                        value: employee.id
+                    })), 'Exit'],
+                },
+            ]).then(answers => {
+                if (answers.employee_id === 'Exit') {
+                    startPrompt();
+                } else {
+                    inquirer.prompt([
+                        {
+                            name: 'role_id',
+                            type: 'list',
+                            message: 'What is the new role for this employee?',
+                            choices: roles.map(role => ({
+                                name: role.title,
+                                value: role.id
+                            })),
+                        }
+                    ]).then(roleAnswer => {
+                        const updateData = {
+                            employee_id: answers.employee_id,
+                            role_id: roleAnswer.role_id
+                        };
+                        employeeModel.updateRole(updateData, (err, results) => {
+                            if (err) throw err;
+                            console.log('Employee role updated!');
+                            startPrompt();
+                        });
+                    });
+                }
+            });
         });
     });
 }
+
  
 
 startPrompt(); 
